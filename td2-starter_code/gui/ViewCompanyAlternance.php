3c6e8fd3f085373f7a7aa4d2538d1682d9bfe748
Alexis FABRE <alexisfabre2006@gmail.com>
2026-03-18 15:07:04 +0100

    Create ViewCompanyAlternance.php based on ViewPost.php

---
+++ td2-starter_code/gui/ViewCompanyAlternance.php
@@ -0,0 +1,10 @@
+<?php
+// File: ViewCompanyAlternance.php
+
+// Include necessary files and configurations
+
+?>
+
+<h1>Company Alternance View</h1>
+<p>This is the view for Company Alternance.</p>
+<a href="/index.php/annoncesAlternance">Return to Annonces Alternance</a>
